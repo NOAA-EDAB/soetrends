f6130b497caee70e6c705adbbf3f3a5f2f23796c
kimberly-bastille <kimberly.bastille@gmail.com>
2021-05-17 17:07:54 -0400

    add rev, rec, zoo, bt, heatwave datasets and add EPU option

--- server.R
+++ server.R
@@ -16,26 +16,6 @@ function(input, output){
     library(grid)
     library(cowplot)
     
-    #GIS libraries
-    #library(sf)
-    #library(rgdal)
-    #library(raster)
-    #library(ggspatial)
-    #library(marmap)
-    
-    
-    #General inline text input for report
-    
-    #Council
-    council <- "Mid-Atlantic Fishery Management Council"
-    council_abbr <- "MAFMC"
-    
-    #Region identifiers
-    epu <- "Mid-Atlantic Bight"
-    epu_abbr <- "MAB"
-    region <- "Mid-Atlantic"
-    region_abbr <- "MA" #Some commercial data organized by "MA" or "NE" regions, not by EPU 
-    
     shade.alpha <- 0.3
     shade.fill <- "lightgrey"
     lwd <- 1
@@ -52,18 +32,38 @@ function(input, output){
     feeding.guilds <- c("Apex Predator","Piscivore","Planktivore","Benthivore","Benthos")
     x.shade.min <- 2010
     x.shade.max <- 2020
-    #ind<- input$Indicator
-    #analysis<- input$`Trend analysis Type`
-    #input$Indicator == Indicator
-    if (input$Indicator == "Landings_Managed") {
-      ### Commercial Landings
+
+    # Managed Revenue ###################
+    if (input$Indicator == "Revenue_Managed") { 
+      apex<-ecodata::hms_landings %>% 
+        dplyr::filter(stringr::str_detect(Var, "Revenue")) %>% 
+        separate(Var, c("Var", "trash"), sep = "_") %>% 
+        group_by(YEAR) %>% 
+        summarise(Value = sum(Value)) %>% 
+        rename( Time = YEAR) %>% 
+        mutate(Var = c("HMS Revenue"), 
+               Units = c("metric tons"), 
+               EPU = c(input$epu_abbr))
+      rev_agg <- ecodata::comdat %>% 
+        dplyr::filter(stringr::str_detect(Var, "Revenue"),
+                      !stringr::str_detect(Var, "Apex|prop|Other|NEFMC"), 
+                      EPU == input$epu_abbr,
+                      Time >= 1986) %>% 
+        rbind(apex) %>% 
+        dplyr::mutate(Status = ifelse(str_detect(Var, "managed"), 
+                                      "Managed","Total")) %>% 
+        dplyr::group_by(Status, Time) %>% 
+        dplyr::summarise(Total = sum(Value)) 
+      ind <- rev_agg %>% dplyr::filter(Status == "Managed") %>% 
+        dplyr::rename(Value = Total)
+      
+      # Managed Landings #############################
+    } else if (input$Indicator == "Landings_Managed") {               
       managed_landings <- ecodata::comdat  %>%
         dplyr::filter(stringr::str_detect(Var, paste0(council_abbr," managed species - Landings weight|JOINT managed species - Landings weight")),
                       !stringr::str_detect(Var, "Other"),
                       Time >= 1986,
-                      EPU == epu_abbr)
-      
-      # HMS Landings
+                      EPU == input$epu_abbr)
       apex<-ecodata::hms_landings %>% 
         dplyr::filter(stringr::str_detect(Var, "Landings")) %>% 
         separate(Var, c("Var", "trash"), sep = "_") %>% 
@@ -72,32 +72,83 @@ function(input, output){
         rename( Time = YEAR) %>% 
         mutate(Var = c("HMS Landings"), 
                Units = c("metric tons"), 
-               EPU = c("MAB"))
-      
-      #Total landings
-      managed_landings <- ecodata::comdat  %>%
+               EPU = c(input$epu_abbr))
+      total_landings <- ecodata::comdat  %>%
         dplyr::filter(!stringr::str_detect(Var, "managed species"),
                       !stringr::str_detect(Var, "Other"),
                       !stringr::str_detect(Var, "Apex"),
                       stringr::str_detect(Var, "Landings"),
                       Time >= 1986,
-                      EPU == epu_abbr) %>% 
+                      EPU == input$epu_abbr) %>% 
         rbind(apex)
-      
       total_landings_agg <- total_landings %>%
         dplyr::group_by(Time) %>%
         dplyr::summarise(Value = sum(Value)) %>% 
         dplyr::mutate(Var = "Total",hline = mean(Value))
-      
       managed_landings_agg <- managed_landings %>%
         dplyr::group_by(Time) %>%
         dplyr::summarise(Value = sum(Value)) %>% 
         dplyr::mutate(Var = "Managed",hline = mean(Value))
-      
       Landings_total <- total_landings_agg
       ind <- managed_landings_agg 
-      } else if (input$Indicator == "LTSST") {
-      ind <- ecodata::long_term_sst 
+      
+      # Recreational Seafood Harvest ########################################
+      } else if (input$Indicator == "Recreational_Seafood_Harvest") {  
+        ind <- ecodata::recdat %>% 
+          dplyr::mutate(EPU = dplyr::recode(EPU, "MA"="MAB") ) %>% 
+          dplyr::filter(EPU == input$epu_abbr,
+                        Var == "Recreational Seafood")
+        
+      # Recreational seafood effort  #######################################
+      } else if (input$Indicator == "Recreational_Effort") { 
+        ind <- ecodata::recdat %>% 
+          dplyr::mutate(EPU = dplyr::recode(EPU, "MA"="MAB") ) %>% 
+          dplyr::filter(EPU == input$epu_abbr, 
+                        Var == "Recreational Effort")
+      
+      # Recreational effort diversity #########################################
+      } else if (input$Indicator == "Recreational_Catch_Diversity") { 
+        ind <- ecodata::recdat %>% 
+          dplyr::mutate(EPU = dplyr::recode(EPU, "MA"="MAB") ) %>% 
+          dplyr::filter(EPU == input$epu_abbr, 
+                        Var == "Recreational Diversity of Catch")
+        
+      # Zooplankton Diversity ##################################################
+      } else if (input$Indicator == "Zooplankton_Diversity"){
+        ind <- ecodata::zoo_diversity %>% 
+          dplyr::filter(EPU == input$epu_abbr)
+        
+      # Bottom Temperature
+      } else if (input$Indicator == "Bottom_Temp"){
+        ind<- ecodata::bottom_temp %>% 
+          dplyr::filter(EPU == input$epu_abbr) %>% 
+          tidyr::complete(Time = tidyr::full_seq(min(bottom_temp$Time):max(bottom_temp$Time),1),
+                          tidyr::nesting(Var)) %>%
+          dplyr::filter(Var == "bottom temp anomaly in situ", 
+                        !Value == "NA")
+        
+      # Heatwave Cumulative Intensity #####################################  
+      } else if (input$Indicator == "Heatwave_Cumulative_Intesity"){
+        ind<- ecodata::heatwave %>% 
+          dplyr::filter(Var == "cumulative intensity", 
+                        EPU == input$epu_abbr) %>% 
+          dplyr::group_by(Time, EPU, Var, Units) %>% 
+          dplyr::summarise(Value = max(Value)) %>% 
+          dplyr::ungroup() 
+        
+      # Heatwave Maximum Intensity #####################################  
+      } else if (input$Indicator == "Heatwave_Maximum_Intesity"){
+        ind<- ecodata::heatwave %>% 
+          dplyr::filter(Var == "maximum intensity", 
+                        EPU == input$epu_abbr)  %>% 
+          dplyr::group_by(Time, EPU, Var, Units) %>% 
+          dplyr::summarise(Value = max(Value)) %>% 
+          dplyr::ungroup() 
+
+      # Long term sst #####################################################
+      } else if (input$Indicator == "LTSST") {                       
+      ind <- ecodata::long_term_sst %>% 
+        dplyr::filter(EPU == input$epu_abbr)
       } else {
       ind <- message("No Data")
       }
@@ -106,54 +157,53 @@ function(input, output){
     p1<- ind %>% 
 
       ggplot2::ggplot()+
-        
-        #Highlight last ten years
-        #ggplot2::annotate("rect", fill = shade.fill, alpha = shade.alpha,
-        #                  xmin = x.shade.min , xmax = x.shade.max,
-        #                  ymin = -Inf, ymax = Inf) +
-        ecodata::geom_gls(aes(x = Time, y = Value))+#,
-                              #group = Var),
-                          #alpha = trend.alpha, size = trend.size) +
-        ggplot2::geom_line(aes(x = Time, y = Value), size = lwd) +
-        ggplot2::geom_point(aes(x = Time, y = Value), size = pcex) +
-        #ggplot2::scale_y_continuous(labels = function(l){trans = l / 1000})+
-        #ggplot2::scale_x_continuous(breaks = seq(1985, 2015, by = 5), expand = c(0.01, 0.01)) +
-        #ggplot2::scale_color_manual(values = series.col, aesthetics = "color")+
-        #ggplot2::guides(color = FALSE) +
+      ecodata::geom_gls(aes(x = Time, y = Value))+
+      ggplot2::geom_line(aes(x = Time, y = Value), size = lwd) +
+      ggplot2::geom_point(aes(x = Time, y = Value), size = pcex) +
       ggplot2::ylab(("Value")) +
       ggplot2::xlab(element_blank())+
-        #ggplot2::geom_hline(aes(yintercept = hline,
-        #                        color = Var),
-        #                    size = hline.size,
-        #                    alpha = hline.alpha,
-        #                    linetype = hline.lty) +
-        ggplot2::ggtitle("Indicator")+
-        ggplot2::theme(axis.title.y = element_text(size = 7))+
-        ecodata::theme_ts()+
-        ecodata::theme_title()
+      ggplot2::ggtitle(paste(input$Indicator,"-",input$epu_abbr))+
+      ggplot2::theme(axis.title.y = element_text(size = 7))+
+      ecodata::theme_ts()+
+      ecodata::theme_title()
     
     p1
     } else if (input$Model == "GAM"){
       
-        gam_norm<- mgcv::gam(Value ~ s(Time), data = ind, na.action = na.omit)#, knots = input$knots, gamma = input$gamma)
-        ind2<- data.frame(pred = predict(gam_norm)) %>% 
-          mutate(Time = c(min(ind$Time):max(ind$Time))) %>% 
-          left_join(ind)
+        gam_norm<- mgcv::gam(Value ~ s(Time, k=input$knots), data = ind, na.action = na.omit, gamma = input$gamma) # calc gam
+        ind2<- data.frame(pred = predict(gam_norm)) %>% # get predicted column
+          mutate(Time = c(min(ind$Time):max(ind$Time))) %>% #add time to predicted
+          left_join(ind) #join with original data
+        #ind2<-gam_norm$model %>% 
+        #  dplyr::rename(pred = "Value") %>% 
+        #  left_join(ind)
+        ### Add derivative calculation!!!!!!!!!!!!!!!!!!! and plotting prep 
         
         p2 <- ind2 %>% 
           ggplot2::ggplot()+
           ggplot2::geom_line(aes(x = Time, y = Value), size = lwd) +
           ggplot2::geom_point(aes(x = Time, y = Value), size = pcex) +
-          ggplot2::geom_line(aes(x = Time, y = pred))+
+          ecodata::geom_gls(aes(x = Time, y = Value), size = lwd+1, alpha = 0.5)+
+          ggplot2::geom_line(aes(x = Time, y = pred), size = lwd+1, color = "gray")+
           ggplot2::ylab(("Value")) +
           ggplot2::xlab(element_blank())+
-          ggplot2::ggtitle("Indicator")+
+          ggplot2::ggtitle(paste(input$Indicator,"-",input$epu_abbr))+
           ggplot2::theme(axis.title.y = element_text(size = 7))+
           ecodata::theme_ts()+
           ecodata::theme_title()
         p2
+        
+        
     } 
+  }) 
+  
+  output$modelSummary<-renderPrint({
+    if (input$Model == "GAM") {
+      gam_norm2<- mgcv::gam(Value ~ s(Time, k=input$knots), data = ind, na.action = na.omit, gamma = input$gamma) 
+      AICcmodavg::AICc(gam_norm2)
+    } else { print("No model: Change model to GAM for value.")}
   })
+  
 }
   
   
--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 shinyUI(
   fluidPage(
-  titlePanel("State of the Ecosystem time series analysis"),
+  titlePanel("State of the Ecosystem Time Series Analysis"),
   
   
   
@@ -10,32 +10,53 @@ shinyUI(
     sidebarPanel(
       
       # Input: Slider for the number of bins ----
-      selectInput(inputId="Indicator",label="choose SOE indicator",choices = c("Landings_Managed"="Landings_Managed","LTSST"="LTSST"),
+      selectInput(inputId="epu_abbr",label="Choose Region",choices = c("MAB"="MAB",
+                                                                        "NE"="NE", 
+                                                                       "GB" = "GB", 
+                                                                       "GOM" = "GOM", 
+                                                                       "All"="All"),
+                  selected = "All",multiple = F),
+      
+      
+      selectInput(inputId="Indicator",label="Choose SOE indicator",choices = c("Revenue_Managed"="Revenue_Managed",
+                                                                               "Landings_Managed"="Landings_Managed", 
+                                                                               "Recreational_Seafood_Harvest" = "Recreational_Seafood_Harvest", 
+                                                                               "Recreational_Effort" = "Recreational_Effort",
+                                                                               "Recreational_Catch_Diversity" = "Recreational_Catch_Diversity",
+                                                                               "Zooplankton_Diversity" = "Zooplankton_Diversity", 
+                                                                               "Bottom_Temp" = "Bottom_Temp", 
+                                                                               "Heatwave_Cumulative_Intensity" = "Heatwave_Cumulative_Intensity", 
+                                                                               "Heatwave_Maximum_Intensity" = "Heatwave_Maximum_Intensity", 
+                                                                               "LTSST"="LTSST"),
                   selected = "LTSST",multiple = F),
       
       selectInput(inputId="Model",label="Choose model type",choices = c("SOE_Standard"="SOE_Standard",
-                                                                        "GAM"="GAM",
-                                                                        "STARS"="STARS"),
+                                                                        "GAM"="GAM"),
                   selected = "SOE_Standard",multiple = F),
       
       sliderInput(inputId = "knots",
                   label = "Number of knots:",
                   min = 0,
-                  max = 10,
-                  value = 1),
+                  max = 30,
+                  value = 1, 
+                  step = 1),
       
       sliderInput(inputId = "gamma",
-                  label = "Smoother Function",
+                  label = "Smoother Function:",
                   min = 0,
-                  max = 5,
-                  value = 1 )
+                  max = 10,
+                  value = 1, 
+                  step = 0.01)
     ),
 
     # Main panel for displaying outputs ----
     mainPanel(
       
       # Output: Histogram ----
-      plotOutput(outputId = "timeseries")
+      plotOutput(outputId = "timeseries"),
+      
+      h4("AIC"),
+      verbatimTextOutput(outputId = "modelSummary")
       #plotOutput(outputId = "distPlot1"),
       #plotOutput(outputId = "distPlot2")
     )

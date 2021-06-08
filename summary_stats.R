4e4103acc17c307ebf1958e570d0ac6871ffe11e
kimberly-bastille <kimberly.bastille@gmail.com>
2021-06-08 15:12:11 -0400

    start stat summary

---
+++ summary_stats.R
@@ -0,0 +1,6 @@
+### Summary Stats for GAM outputs
+
+# For each of the indicators run through multiple knot and smoother combiations to visualise where GAMs show relatively fixed outputs. 
+
+# Plots showing number of local AIC minimums
+
